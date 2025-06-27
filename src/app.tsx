f2eb1d5d41f3bf4ba7438c85e8d9fc996f3d1bfb
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-06-27 18:57:34 +0900

    starting

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import PWABadge from "./PWABadge.tsx";
 import "./app.css";
+import ArrayRenderer from "./components/_devPurpose/arrayRenderer/index.tsx";
 // import Routing from "./components/_devPurpose/routing/index.tsx";
 // import Showcases from "./components/_devPurpose/showcases/index.tsx";
 import RndTester from "./components/_devPurpose/rnd/index.tsx";
@@ -8,6 +9,7 @@ export default function App() {
   // todo: This is only for dev.
   return (
     <>
+      {/* <ArrayRenderer /> */}
       <RndTester />
       {/* {(() => {
         switch (slug) {
