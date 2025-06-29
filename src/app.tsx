d4e0ad7f0dba24417f30ba990802de0f8a63ac42
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-06-29 17:58:02 +0900

    no overengineering, but save them for later

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,12 @@
 import PWABadge from "./PWABadge.tsx";
 import "./app.css";
-import ArrayRenderer from "./components/_devPurpose/testRenderers/ArrayRenderer.tsx";
-// import Routing from "./components/_devPurpose/routing/index.tsx";
-// import Showcases from "./components/_devPurpose/showcases/index.tsx";
 import RndTester from "./components/_devPurpose/rnd/index.tsx";
-import ObjectRenderer from "./components/_devPurpose/testRenderers/ObjectRenderer.tsx";
-import TestRenderers from "./components/_devPurpose/testRenderers/index.tsx";
-import { Profiler } from "react";
 
 export default function App() {
   // todo: This is only for dev.
   return (
     <>
-      <TestRenderers />
-      {/* <RndTester /> */}
+      <RndTester />
       {/* {(() => {
         switch (slug) {
           default:
