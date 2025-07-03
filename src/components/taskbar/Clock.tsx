e8f3c51e80962332de057e2b8aa7142a6a37b312
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-07-03 13:10:03 +0900

    Clocklogic

---
+++ src/components/taskbar/Clock.tsx
@@ -0,0 +1,5 @@
+function Clock() {
+  return <div className="px-2 text-xs">오후 2:15</div>;
+}
+
+export default Clock;
--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -2,6 +2,7 @@ import useIsMobile from "@/hooks/useIsMobile";
 import { RndWindowEntriesType } from "../programs/types";
 import { Button } from "../ui/Button";
 import { useHasIOSBottomBar } from "@/hooks/useIsIOSNotch";
+import Clock from "./Clock";
 
 // currently for testing
 export default function Taskbar({
@@ -66,7 +67,7 @@ export default function Taskbar({
       </ol>
       <div className="p-0.5">
         <div className="shrink-0 p-0.5 shadow-[inset_-1.5px_-1.5px_0_0_#fcfcfc,_inset_1.5px_1.5px_0_0_#a099a1] h-[26px] flex items-center">
-          <time className="px-2 text-xs">오후 2:15</time>
+          <Clock />
         </div>
       </div>
     </nav>
