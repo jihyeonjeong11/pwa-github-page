72a357780ec389995e46c3c8af9939f43ca3b54a
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-07-09 17:13:18 +0900

    base useWallpaper

--- src/components/Desktop/index.tsx
+++ src/components/Desktop/index.tsx
@@ -1,10 +1,28 @@
 // todo : 바탕화면 -> 월페이퍼 훅
 // todo : programlist -> icon
 
+import useWallpaper from "@/hooks/useWallpaper";
+import Taskbar from "../taskbar";
+
+// todo: 윈도우 로딩 스크린
 // todo: taskbar -> 시작메뉴 dropdown
 
 function Desktop() {
-  return <div>hello</div>;
+  const { style, getRandomWallpaper } = useWallpaper();
+  return (
+    <div
+      id="app"
+      style={style}
+      onContextMenu={(e) => {
+        // todo: 모바일 어케햇더라
+        e.preventDefault();
+        getRandomWallpaper();
+      }}
+    >
+      123
+      <Taskbar />
+    </div>
+  );
 }
 
 export default Desktop;
